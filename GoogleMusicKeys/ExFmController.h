2bfc4c04931a4a609ee74c2cc3671bc0c9f51a12
Joshua Halickman <jhalickman@gmail.com>
2011-11-25 00:20:05 -0500

    Missed new files

---
+++ GoogleMusicKeys/ExFmController.h
@@ -0,0 +1,16 @@
+//
+//  ExFmController.h
+//  GoogleMusicKeys
+//
+//  Created by Joshua Halickman on 11/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "MusicControllerBase.h"
+
+@interface ExFmController : MusicControllerBase {
+
+}
+
+@end
---
+++ GoogleMusicKeys/ExFmController.m
@@ -0,0 +1,38 @@
+//
+//  ExFmController.m
+//  GoogleMusicKeys
+//
+//  Created by Joshua Halickman on 11/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ExFmController.h"
+
+
+#define PLAY_PAUSE_COMMAND @"AudioPlayer.PlayPause();"
+#define NEXT_COMMAND @"AudioPlayer.Next();"
+#define PREVIOUS_COMMAND @"AudioPlayer.Previous();"
+#define SHUFFLE_COMMAND @""
+
+@implementation ExFmController
+
+-(void) playPause {
+    [self tryChromeThenSafari:PLAY_PAUSE_COMMAND];
+}
+
+-(void) next {
+    [self tryChromeThenSafari:NEXT_COMMAND];
+}
+
+-(void) previous {
+    [self tryChromeThenSafari:PREVIOUS_COMMAND];
+}
+
+-(void) shuffle {
+    [self tryChromeThenSafari:SHUFFLE_COMMAND];
+}
+
+-(NSString *)getTabName {
+	return @"exfm";
+}
+@end
