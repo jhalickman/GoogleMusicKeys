c54ee12fcb466484064683815aed1db94f6fa51c
Eyuel Tessema <eyueltessema@gmail.com>
2013-03-30 03:54:12 -0700

    Fix warnings

--- ShortcutRecorder/SRCommon.h
+++ ShortcutRecorder/SRCommon.h
@@ -112,7 +112,7 @@ enum {
 
 // Macros for glyps
 #define SRInt(x) [NSNumber numberWithInteger:x]
-#define SRChar(x) [NSString stringWithFormat: @"%C", x]
+#define SRChar(x) [NSString stringWithFormat: @"%d", x]
 
 // Some default values
 #define ShortcutRecorderEmptyFlags 0
